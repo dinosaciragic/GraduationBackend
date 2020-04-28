1fbbb8aae2e05b51d67bc746bff41584e68b8d6b
Dino Saciragic <dino@appit.ba>
2020-04-28 20:31:41 +0200

    fix infi

--- routes/orders.js
+++ routes/orders.js
@@ -6,17 +6,18 @@ const Order = require('../models/Order');
 
 // api/orders/add
 router.post('/add', verify, (req, res) => {
-    const { items, location, userId, deliveryTime, contctPhone, itemsCost, totalCost, isStarted } = req.body;
+    const { items, location, userId, deliveryTime, contactPhone, itemsCost, totalCost, isStarted, notes } = req.body;
 
     const newOrder = new Order({
         items,
         location,
         userId,
         deliveryTime,
-        contctPhone,
+        contactPhone,
         itemsCost,
         totalCost,
-        isStarted
+        isStarted,
+        notes
     });
 
     // Save order
@@ -34,5 +35,35 @@ router.post('/userOrders', verify, async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+router.post('/workerOrders', verify, paginatedResults(Order), (req, res) => {
+    res.send(res.paginatedResults);
+});
+
+router.put('/:id', verify, (req, res, next) => {
+    Order.findByIdAndUpdate({ _id: req.params.id }, req.body).then((updatedOrder) => {
+        res.send(updatedOrder);
+    });
+});
+
+// Pagination middleware
+function paginatedResults(model) {
+    return async (req, res, next) => {
+        const page = req.query.page;
+        const limit = 12;
+
+        const startIndex = (page - 1) * limit;
+
+        try {
+            const results = await model.find({ isStarted: false }).limit(limit).skip(startIndex).exec();
+            res.paginatedResults = results;
+            next();
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+
+
+    }
+}
 
 module.exports = router;
