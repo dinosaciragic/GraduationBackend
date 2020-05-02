f5dd70debce57b767c5736eadb62f4d731e69d2b
Dino Saciragic <dino@appit.ba>
2020-05-02 16:50:50 +0200

    orders done

--- routes/orders.js
+++ routes/orders.js
@@ -37,9 +37,14 @@ router.post('/userOrders', verify, async (req, res) => {
     }
 });
 
-// get worker orders paged
+// get worker orders for home page/dashboard paged
 router.post('/workerOrders/:page', verify, paginatedResults(Order), (req, res) => {
     res.send(res.paginatedResults);
+});
+
+// get worker orders by id for worker
+router.get('/workerOrders/:id', verify, getWorkerOrders(), (req, res) => {
+    res.send(res.workerOrders);
 });
 
 // Get order by id
@@ -50,9 +55,22 @@ router.get('/single/:id', verify, (req, res) => {
 });
 
 // edit order
-router.put('/edit/:id', verify, (req, res, next) => {
-    Order.findByIdAndUpdate({ _id: req.params.id }, req.body).then((updatedOrder) => {
-        res.send({ updatedOrder: true });
+router.put('/edit/:id', verify, async (req, res, next) => {
+    const alreadyTaken = await Order.find({ workerId: req.body.workerId });
+  
+    if (alreadyTaken.length < 2) {
+        Order.findByIdAndUpdate({ _id: req.params.id }, req.body).then((updatedOrder) => {
+            res.send({ updatedOrder: true });
+        });
+    } else {
+        res.send({ msg: "You can have a max of two taken orders at a time" });
+    }
+});
+ 
+// Delete order API
+router.delete('/delete/:id', verify, (req, res, next) => {
+    Order.findByIdAndDelete({ _id: req.params.id }).then((updatedOrder) => {
+        res.send({ deletedOrder: true });
     });
 });
 
@@ -73,6 +91,18 @@ function paginatedResults(model) {
         }
 
 
+    }
+}
+
+function getWorkerOrders() {
+    return async (req, res, next) => {
+        try {
+            const results = await Order.find({ workerId: req.params.id });
+            res.workerOrders = results;
+            next();
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
     }
 }
 
