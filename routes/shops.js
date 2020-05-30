ce2ae3fbb85f52246b721d0302e4d8cb41e1e022
Dino Saciragic <dino@appit.ba>
2020-05-30 18:27:28 +0200

    shops vackend done

--- routes/shops.js
+++ routes/shops.js
@@ -5,9 +5,10 @@ const verify = require('./verifytoken');
 const ShopItem = require('../models/ShopItem');
 
 // api/shops?page=1
-router.get('/', verify, (req, res) => {
-    res.send("works");
+router.get('/', verify, paginatedResults(ShopItem), (req, res) => {
+    res.send(res.paginatedResults);
 });
+
 
 // api/shops/add
 router.post('/add', verify, (req, res) => {
@@ -25,5 +26,29 @@ router.post('/add', verify, (req, res) => {
     }).catch(err => console.error(err));
 
 });
+
+// Pagination middleware
+function paginatedResults(model) {
+    return async (req, res, next) => {
+        const page = req.query.page;
+        const search = req.query.searchTerm;
+        const shopType = req.query.shopType;
+        const limit = 12;
+        var regex = new RegExp(search, 'i');  // RegEdp works as contains for find function and 'i' makes it case insensitive
+        const startIndex = (page - 1) * limit;
+
+        try {
+
+            const results = await model.find({ name: regex, shopType: shopType }).limit(limit).skip(startIndex).exec();
+            res.paginatedResults = results;
+            next();
+
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+
+
+    }
+}
 
 module.exports = router;
